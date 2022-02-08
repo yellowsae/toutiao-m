91d846fc7439f1d63dbef6c118640a793dd867be
yellowsae <yunhai0644@163.com>
2022-02-08 23:13:05 +0800

    开始开发首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home-container">
-    首页
+    <!--    导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+<!--      添加按钮 -->
+      <van-button
+      class="search-btn"
+      round
+      size="small"
+      type="info"
+      icon="search"
+      slot="title"
+      >搜索
+      </van-button>
+    </van-nav-bar>
   </div>
 </template>
 
@@ -10,6 +22,20 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: unset;
+  }
 
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background-color: #5babfb;
+    font-size: 14px;
+    .van-icon {
+      font-size: 16px;
+    }
+  }
+}
 </style>
